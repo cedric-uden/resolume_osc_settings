0e016f34f78fbaaaeec9f033a61668774eaed0eb
Cédric Uden <cedric@uden.de>
2023-12-06 01:41:14 +0100

    added bools for black and solo

--- src/roset/structs/layer_configuration.py
+++ src/roset/structs/layer_configuration.py
@@ -20,6 +20,8 @@ class LayerConfiguration(ConfigMasterAbc):
         audio_level: float = 1.0,
         master_fader: float = 1.0,
         speed: float = 0.25,
+        black: bool = False,
+        solo: bool = False,
         transition_duration: float = 0.0,
         transition_type: Transition = Transition.ALPHA,
         transport_playdirection: PlayDirection = PlayDirection.PLAY,
@@ -29,6 +31,8 @@ class LayerConfiguration(ConfigMasterAbc):
         self.audio_level = audio_level
         self.master_fader = master_fader
         self.speed = speed
+        self.black = black
+        self.solo = solo
         self.transition_duration = transition_duration
         self.transition_type = transition_type
         self.transport_playdir = transport_playdirection
@@ -39,6 +43,8 @@ class LayerConfiguration(ConfigMasterAbc):
             ConfigItem("audio/volume", self.audio_level, float),
             ConfigItem("master", self.master_fader, float),
             ConfigItem("speed", self.speed, float),
+            ConfigItem("bypassed", self.black, int),
+            ConfigItem("solo", self.solo, int),
             ConfigItem("clips/*/transport/position/behaviour/speed", self.speed, float),
             ConfigItem("transition/duration", self.transition_duration, duration_seconds_to_float),
             ConfigItem("video/transition/mixer/blendmode", self.transition_type.value),
